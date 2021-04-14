94fcba78446a84623a29fb364a3715a7489837ef
陈继亮 <chenjl8891@126.com>
2021-04-14 09:46:25 +0800

    更新目录结构

--- tools/apollo_manage.py
+++ tools/apollo_manage.py
@@ -7,13 +7,18 @@ import requests
 
 ENV = "dev"
 URL = "http://configserver-{env}.chj.cloud/configs/{appName}/default/application"
-
-env = os.getenv("RUNTIME_ENV") or ENV
+ALL_APOLLO_COFNIG = dict()
 
 def get_apollo_config(k, appName):
+    return ALL_APOLLO_COFNIG(k, "")
+
+
+def get_apollo_all_config(appName):
+    env = os.getenv("RUNTIME_ENV") or ENV
     data = requests.get(URL.format(env=env, appName=appName))
 
     if data.status_code == 200:
-        return data.json()["configurations"][k]
+        ALL_APOLLO_COFNIG = data.json()["configurations"]
+        return ALL_APOLLO_COFNIG
     else:
-        return ""
+        return dict()
